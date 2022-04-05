6909dc5b62f8e40dfcbcab2502fd9299b80cf733
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-05 14:54:02 -0400

    commit tree 132bb77d2d7a1d1fd737722535d4bd2b246c34da

--- src/index.js
+++ src/index.js
@@ -12,4 +12,44 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  cardImages()
 });
+
+function cardImages() {
+  fetch("http://localhost:3000/toys")
+    .then(response => response.json())
+    .then(data => data.forEach(card => renderToys(card)))
+}
+function renderToys(toys) {
+  console.log(toys)
+  let collection = document.getElementById(`toy-collection`)
+  let h2 = document.createElement("h2")
+  // toyName = document.getElementsByClassName(`input-text`)
+  let img = document.createElement("img")
+  img.src = toys.image
+  img.class = "toy-avatar"
+
+  h2.innerText = toys.name
+
+  let p = document.createElement("p")
+  p.innerText = toys.likes
+
+
+  let button = document.createElement("button")
+  button.class = "like-btn"
+  button.id = "[toy_id]"
+  button.innerText = "Like ❤️"
+
+  button.addEventListener(`click`, () => {
+    toys.likes += 1
+    p.innerText = toys.likes
+  })
+
+  // collection.append(h2)
+  let toyDiv = document.createElement("div")
+  toyDiv.class = "card"
+  toyDiv.append(h2, img, p, button)
+  collection.append(toyDiv)
+}
+
+
